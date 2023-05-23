64095c6546aa2c0f5ec73c566a7ab059c5b4a179
Morgan Overman <Smorgan06@gmail.com>
2023-05-23 19:45:07 -0400

    Add files via upload

---
+++ testing.ps1
@@ -0,0 +1,34 @@
+# Importing
+#import .\mylib.ps1
+
+
+# Types
+$str = "Test"
+$bool = True
+$mychar = 'T'
+$myint = 22
+$myfloat = 22.75
+
+# conditions
+#if
+#if ($x == 1 -and $x == 2) {}
+#elseif
+#else
+
+# Lists
+#$List = [System.Collections.ArrayList]::new()
+
+# Arrays
+#$array = @("item", "item2")
+
+# Dictionaries / HashTables
+#$newDict = @{}
+#$newDict.Add("Key", "Value")
+
+# Loops
+# foreach ($item in $list){
+#     write(x)
+# }
+# while ($true){
+#     write(x)
+# }
